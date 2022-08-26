80344ee8b8d80296a91acef03cf41015b3363909
Arthur <arthur.albert990@gmail.com>
2022-08-26 15:09:49 -0300

    Add player and computer choice function.

--- main.js
+++ main.js
@@ -1,1 +1,45 @@
-console.log("hell3o")
+//[OK] usuário escolhe pedra papel ou tesoura
+//[OK] computador escolhe pedra papel ou tesoura aleatoriamente
+//[EM ANDAMENTO] comparar escolha do usuario e do computador
+//exibe mensagem com o resultado
+
+
+function getComputerChoice(){
+    let options = ["Rock","Paper", "Scissors"]
+
+    let computerChoice = Math.floor(Math.random() * (options.length))
+
+    return options[computerChoice];
+}
+
+function getUserChoice(){
+    return userChoice = prompt("Rock, Paper or Scissors?");
+}
+
+function playRound(playerSelection = getUserChoice(), computerSelection = getComputerChoice()){
+    let pS = playerSelection.toLowerCase();
+    let cS = computerSelection.toLowerCase();
+
+    if(pS===cS){
+        return "TIE!"
+    }else{
+        switch(pS){
+            case "rock":
+                if (cS==="paper"){
+                    return "You LOOSE!"
+                } else {
+                    return  "You WIN!"
+                }
+            case "paper":
+                if(cS==="rock"){
+                    return "You LOOSE!"
+                } 
+                else{
+                    return "You WIN!"
+                }
+        }
+    }
+}
+
+
+
