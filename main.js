ecc1a15639a8e8589dbf5d43312e5591408a2177
Arthur <arthur.albert990@gmail.com>
2022-08-29 08:32:11 -0300

    Add game function, update playRound function

--- main.js
+++ main.js
@@ -19,33 +19,44 @@ function getUserChoice(){
 function playRound(playerSelection = getUserChoice(), computerSelection = getComputerChoice()){
     let pS = playerSelection.toLowerCase();
     let cS = computerSelection.toLowerCase();
-    console.log([pS, cS]);
+    let roundChoices = [pS, cS];
     
     if(pS===cS){
-        return "TIE!"
+        return ["TIE!", roundChoices];
+
     } else{
         switch(pS){
             case "rock":
                 if (cS==="paper"){
-                    return "You LOOSE!"
+                    return [`You LOOSE, ${cS} beats ${pS}!`,roundChoices];
                 } else {
-                    return  "You WIN!"
+                    return [`You WIN, ${pS} beats ${cS}!`,roundChoices];
                 }
             case "paper":
-                if(cS==="rock"){
-                    return "You LOOSE!"
+                if(cS==="scissors"){
+                    return [`You LOOSE, ${cS} beats ${pS}!`,roundChoices];
                 } else{
-                    return "You WIN!"
+                    return [`You WIN, ${pS} beats ${cS}!`,roundChoices];
                 }
             case "scissors":
                 if(cS==="rock"){
-                    return "You LOOSE!"
+                    return [`You LOOSE, ${cS} beats ${pS}!`,roundChoices];
                 } else{
-                    return "You WIN!"
+                    return [`You WIN, ${pS} beats ${cS}!`,roundChoices];
                 }
             }
         }
     }
 
 
+function game(rounds){
+    let fullGameResult = []
 
+    for (let x =1; x<=rounds; x++){
+        let roundResult = playRound()
+        fullGameResult.push(roundResult[1]);
+        window.alert(roundResult[0]);
+    }
+
+    return fullGameResult;
+}
